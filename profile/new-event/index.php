8f103457587ae4d240976f5d6b21c603f0c25d98
evanswanjau <evanswanjau@gmail.com>
2018-04-14 14:49:28 +0300

    New event upload ui

---
+++ profile/new-event/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Upload new event</title>
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap-theme.min.css">
+    <script type="text/javascript" src="../../js/bootstrap.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.js"></script>
+    <script type="text/javascript" src="../../js/script.js"></script>
+    <link rel="stylesheet" href="../../css/jquery-ui.min.css">
+    <script src="../../js/jquery-ui.min.js"></script>
+  </head>
+  <body>
+    <div class="container-form">
+      <form class="ui-form" action="" method="post">
+        <h1>upload new event</h1>
+        <input type="text" name="name" placeholder="event name"><br><br>
+        <input type="text" name="description" placeholder="description"><br><br>
+        <select class="" name="event">
+          <option value="Weddings">Weddings</option>
+          <option value="Sports">Sports</option>
+          <option value="Art">Art</option>
+        </select><br><br>
+        <input type="text" name="venue" placeholder="venue"><br><br>
+        <input type="submit" name="post-event" value="post event">
+      </form>
+    </div>
+  </body>
+</html>
